56e2859067c1171a9aad8b4e851f58016b7e8cf8
Nicolle <nickkoetzler@gmail.com>
2022-04-08 17:04:14 -0300

    virar cartas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const cartas = ["bobrossparrot.gif", "explodyparrot.gif", "fiestaparrot.gif", "metalparrot.gif", "revertitparrot.gif" ]
+
 let quantidadeCartas = parseInt(prompt("Com quantas cartas deseja jogar? (4, 6, 8, 10, 12 ou 14)"));
 
 while(quantidadeCartas % 2 !== 0 || quantidadeCartas < 4 || quantidadeCartas > 14) {
@@ -10,14 +12,25 @@ function aparecerCartas(numeroCartas) {
         let cartas = document.querySelector(".bottom");
         cartas.innerHTML += 
         `<div class="par">
-        <div class="cards1">
-            <img class="front" src="imgs/front.png" alt="">
+        <div class="cards1" onclick="virarCartas(this)">
+            <div class="back face"></div>
+            <div class="front face">
+                <img src="imgs/front.png">
+            </div>
         </div>
-        <div class="cards2">
-            <img class="front" src="imgs/front.png" alt="">
+        <div class="cards1" onclick="virarCartas(this)">
+            <div class="back face"></div>
+            <div class="front face">
+                <img src="imgs/front.png">
+            </div>
         </div>
-        </div>`;
+    </div>`;
     contador ++;
     }
 }
 aparecerCartas(quantidadeCartas);
+
+function virarCartas(elemento) {
+    elemento.querySelector(".front").classList.add("frontflip")
+    elemento.querySelector(".back").classList.add("backflip")
+}
